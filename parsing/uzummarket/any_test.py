ce579338be5a4674ea7384b77058dc8d2630f50b
ChoriyevJamshid <qpq777111000@gmail.com>
2024-05-21 20:59:41 +0500

    add any_test.py for testing selenium for ubuntu server

--- parsing/uzummarket/any_test.py
+++ parsing/uzummarket/any_test.py
@@ -6,10 +6,12 @@ options.add_argument('--no-sandbox')
 options.add_argument("--headless")
 options.add_argument('--disable-dev-shm-usage')
 import time
-
+st = time.perf_counter()
 url = f'https://uzum.uz'
 
 browser = webdriver.Chrome(options=options)
 browser.get(url)
 time.sleep(10)
+en = time.perf_counter()
+print(f"Time: {round(en - st, 3)}")
 print("ALL WORKS OK!!!")
