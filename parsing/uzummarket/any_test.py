3b17a429ba3123c6983e73ad17099c917a3f5572
ChoriyevJamshid <qpq777111000@gmail.com>
2024-05-21 20:46:10 +0500

    add any_test.py for testing selenium for ubuntu server

--- parsing/uzummarket/any_test.py
+++ parsing/uzummarket/any_test.py
@@ -1,8 +1,15 @@
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument("--headless")
+options.add_argument('--disable-dev-shm-usage')
 import time
 
 url = f'https://uzum.uz'
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(options=options)
 browser.get(url)
 time.sleep(10)
+print("ALL WORKS OK!!!")
