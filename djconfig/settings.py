09b6047bf24edf18e7fa4753bb5b880dc8526748
ChoriyevJamshid <qpq777111000@gmail.com>
2024-05-22 00:14:54 +0500

    ...

--- djconfig/settings.py
+++ djconfig/settings.py
@@ -19,8 +19,10 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
-
+DEBUG = os.environ.get("DEBUG", False)
+print('\n\n')
+print(f'DEBUG = {DEBUG}')
+print('\n\n')
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
