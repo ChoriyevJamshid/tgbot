a3b12ba33cba34718d05b882c6129b569a06f6da
ChoriyevJamshid <qpq777111000@gmail.com>
2024-05-26 20:15:33 +0500

    Commit

--- djconfig/settings/prod.py
+++ djconfig/settings/prod.py
@@ -9,10 +9,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRES_HOST", 'localhost'),
-        "NAME": os.environ.get('POSTGRES_NAME', 'postgres'),
-        "USER": os.environ.get("POSTGRES_USER", 'postgres'),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", 'postgres'),
+        "HOST": os.environ.get("POSTGRES_HOST", '127.0.0.1'),
+        "NAME": os.environ.get('POSTGRES_NAME', 'dtb_db'),
+        "USER": os.environ.get("POSTGRES_USER", 'dtb_user'),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", 'dtb_user'),
         "PORT": os.environ.get("POSTGRES_PORT", '5432')
     }
 }
