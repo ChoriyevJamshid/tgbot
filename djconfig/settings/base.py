26baabf77e548d693ea080b82947c913981b8ead
ChoriyevJamshid <qpq777111000@gmail.com>
2024-05-26 21:37:50 +0500

    ..

--- djconfig/settings/base.py
+++ djconfig/settings/base.py
@@ -103,6 +103,19 @@ DB_POSTGRESQL = 'postgresql'
 #     "default": dj_database_url.config(conn_max_age=600, default='sqlite:///db.sqlite3')
 # }
 
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "NAME": os.environ.get('POSTGRES_DB'),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "PORT": os.environ.get("POSTGRES_PORT")
+    }
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- djconfig/settings/prod.py
+++ djconfig/settings/prod.py
@@ -6,26 +6,16 @@ MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
 # DATABASES = {
 #     'default': {
 #         "ENGINE": "django.db.backends.postgresql",
-#         "HOST": os.environ.get("POSTGRES_HOST"),
-#         "NAME": os.environ.get('POSTGRES_DB', 'dtb_db'),
-#         "USER": os.environ.get("POSTGRES_USER", 'dtb_user'),
-#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", 'dtb_user'),
-#         "PORT": os.environ.get("POSTGRES_PORT", '5432')
+#         "NAME": 'postgres',
+#         "USER": 'postgres',
+#         "PASSWORD": 'postgres',
+#         "HOST": 'db',
+#         "PORT": 'postgres'
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'postgres',
-        "USER": 'postgres',
-        "PASSWORD": 'postgres',
-        "HOST": 'db',
-        "PORT": 'postgres'
-    }
-}
 
 
