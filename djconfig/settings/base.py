94519a0e0ceebc8aa37283e8e5f36785077b7b7e
ChoriyevJamshid <qpq777111000@gmail.com>
2024-05-26 21:45:20 +0500

    ..

--- djconfig/settings/base.py
+++ djconfig/settings/base.py
@@ -106,7 +106,6 @@ DB_POSTGRESQL = 'postgresql'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRES_HOST"),
         "NAME": os.environ.get('POSTGRES_DB'),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
